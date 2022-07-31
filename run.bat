797e74746c4e0833ba3537b90cf330f688bc37e8
max2556 <46759632+max2556@users.noreply.github.com>
2022-07-31 23:35:05 +0300

    SERVER CLOSED: 31.07.2022

--- run.bat
+++ run.bat
@@ -3,7 +3,6 @@ REM Add custom JVM arguments to the user_jvm_args.txt
 REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
 REM  pass them to this script directly
 ./init
-
 java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.2-40.1.54/win_args.txt -nogui %*
 echo "SERVER CLOSED!!!"
 echo "PRESS ANY BUTTON!"
